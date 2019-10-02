5eb2b2c5e80a31b76c0503c5fdb2e24f8c1c142c
Adriano Galvão <adrianogalvao@agbtecnologia.com.br>
2019-10-01 23:07:02 -0300

    DepartmentService

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -45,6 +45,7 @@ namespace SalesWebMvc
                     builder => builder.MigrationsAssembly("SalesWebMvc")));
 
             services.AddScoped<SellerService>();
+            services.AddScoped<DepartmentService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
